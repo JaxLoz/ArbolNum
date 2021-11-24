06eb152523e2f281b2f48865b6c6ea4dae8f1e38
JavirKali <jaxloz2002@gmail.com>
2021-11-23 22:16:37 -0500

    Ultima actualizacion al repositorio antes de guardar

--- src/ITiporecorido.java
+++ src/ITiporecorido.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author javier
- */
-public class ITiporecorido {
+@FunctionalInterface
+public interface ITiporecorido {
+    
+    public void Recorrido (Nodo nod);
     
 }
