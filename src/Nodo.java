a724b58bf8b3f083f633e9dff154e2ceb7a3167c
JavirKali <jaxloz2002@gmail.com>
2021-11-23 20:37:10 -0500

    Se agrego y se implemento la clase Nodo.java

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,4 +1,111 @@
+import javax.swing.tree.DefaultMutableTreeNode;
 
-public class Nodo {
+public class Nodo extends DefaultMutableTreeNode {
+    
+    private boolean NIzquierdo;
+    
+    public Nodo (){
+        
+        this("");
+    }
+    
+    public Nodo (String txt){
+        
+        super(txt);
+        NIzquierdo = true; 
+    }
+    
+    public void setNIzquierdo (boolean Niz){
+        
+        this.NIzquierdo = Niz;
+    }
+    
+    public boolean getNIzquierdo (){
+        
+        return this.NIzquierdo;
+    }
+    
+    public Nodo BuscarNodo (boolean Niz){
+        
+        Nodo nod;
+        nod = null;
+        
+        for (int i = 0; i < getChildCount(); i++){
+            
+            nod = (Nodo)getChildAt(i);
+            
+            if (nod.getNIzquierdo() == Niz){
+                break;
+            }else{
+                
+                nod = null;
+            }
+        }
+        
+        return nod;
+    }
+    
+    public Nodo HijoNizquierdo (){
+        
+        return BuscarNodo (true);
+    }
+    
+    public Nodo HijoNderecho (){
+        
+        return BuscarNodo (false);
+    }
+    
+    public boolean AñadirNIz (Nodo nod){
+        
+        if (HijoNizquierdo () == null){
+            
+            nod.setNIzquierdo(true);
+            add (nod);
+            return true;
+        }else{
+            
+            return false;
+        }
+    }
+    
+    public boolean AñadirNder (Nodo nod){
+        
+        if (HijoNderecho () == null){
+            
+            nod.setNIzquierdo(false);
+            add(nod);
+            return true;
+        }else {
+            
+            return false;
+        }
+    }
+    
+    public String getIndicador (){
+        return getUserObject() + " ";
+    }
+    
+    @Override
+    public String toString (){
+        
+        String res = getIndicador();
+        
+        if (getParent () == null){
+            
+            return res;
+            
+        }else{
+            if (NIzquierdo){
+                return res + "(Iz)"; 
+                        
+            }else{
+                return res + "(De)";
+            }
+        }
+    }
     
 }
+
+
+
+
